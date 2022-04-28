45dec3b3640d8a67f158d253303c18d9e59dc3f9
Toby Allen <toby@cookingisfun.ie>
2022-04-28 06:46:06 +0100

    Raise exception if id not available

--- src/AnyModel.php
+++ src/AnyModel.php
@@ -8,22 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class AnyModel extends Model
 {
-    
+
     public $timestamps = false;
 
-    public static function table($tablename, $idfield = null, $options = []){
-        
+
+    public static function table($tablename, $options = []){
+
         // Create a new Model  and set its table and primary key.
         $Model = new AnyModel();
-        $Model->table = $tablename;        
-        $Model->primaryKey = $idfield;
-        
-        // Set any further options passed in before returning         
-        Collect($options)->each(function($value, $name){
-            $this->{$name} = $value;
-        });        
-        
+        $Model->table = $tablename;
+
+        // Set any further options passed in before returning
+        Collect($options)->each(function($value, $name) use ($Model){
+            $Model->{$name} = $value;
+        });
+
         return $Model;
     }
-    
+    public function getIDattribute(){
+        // due to various contortions Model->id will not always be available and it is not possible to set
+        // primaryKey and have it work so if it is requested and does not exist rather than returning '' (default behaviour)
+        // raise an error.
+        if (!isset($this->{$this->primaryKey})){
+            throw new \Exception('AnyModel::id does not have a value.  Please specify actual id field name');
+        }
+
+    }
+
 }
